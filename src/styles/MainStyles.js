d57c91beb3d5ff63c1b30eb5f1cb86684592e311
endoresur <mlhv2lv@gmail.com>
2022-01-22 22:43:33 +0500

    Add parent container with changeable properties

--- src/styles/MainStyles.js
+++ src/styles/MainStyles.js
@@ -4,32 +4,36 @@ const MainContainer = styled.div`
   width: 100%;
   height: 100vh;
   
-  background-image: linear-gradient(#f37bbb, #595ef5);
-
-  
+  background-image: linear-gradient(#f37bbb, #595ef5);  
 `
 
-const Container = styled.div`
-  position: relative;
+const ParentContainer = styled.div`
   
-  margin-top: ${props => props.mt || ''};  
+  margin-top: ${props => props.mt || ''};
   margin-bottom: ${props => props.mb || ''};
   margin-left: ${props => props.ml || ''};
   margin-right: ${props => props.mr || ''};
-  
-  font-size: ${props => props.fs || '1em'};
-`
-
-const GridContainer = styled.div`
-  display: grid;
-  grid-template-columns: ${props => props.columns || ''};
-  grid-template-rows: ${props => props.rows || ''};
   
   border-right: ${props => props.br || '' };
   border-left: ${props => props.bl || '' };
   border-top: ${props => props.bt || '' };
   border-bottom: ${props => props.bb || '' };
-  border: ${props => props.b || ''};
+  border: ${props => props.b || ''}; 
+    
+`
+
+const Container = styled(ParentContainer)`
+  position: relative;
+  
+  font-size: ${props => props.fs || '1em'};
+`
+
+const GridContainer = styled(ParentContainer)`
+  display: grid;
+  grid-template-columns: ${props => props.columns || ''};
+  grid-template-rows: ${props => props.rows || ''};
+  
+  align-items: center;
 
   font-size: ${props => props.fs || '1em'};
   
